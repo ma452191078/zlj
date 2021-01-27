77e1ca3020ff924c2f847cc3977eb92fa8fa3360
majingyuan <jnmajingyuan@163.com>
2021-01-27 15:21:48 +0800

    总分页面修改

--- common/js/base.js
+++ common/js/base.js
@@ -2,16 +2,18 @@
  * Created by majingyuan on 2017/5/29.
  */
 // 正式系统
- var basePath1 = 'http://weixin.shidanli.cn:8081';
- var basePath = 'http://140.249.23.57:8083';
+ var basePath = 'http://weixin.shidanli.cn:8081';
+ var basePath1 = 'http://140.249.23.57:8083';
+ // var basePath1 = 'http://localhost:8080';
 
 //测试系统
-// var basePath = 'http://localhost:8088';
+// var basePath = 'http://localhost:8080';
+// var actionName = "";
 var actionName = "/qualityprize";
 var projectName = '/zlj';
 var path = basePath + actionName;
-var mPath = basePath + actionName;
-var imgUrl = basePath1 + projectName;
+var mPath = basePath1 + actionName;
+var imgUrl = basePath + projectName;
 var qrUrl = imgUrl + '/mobile/index.html?gameId=';
 var loginUrl = "http://" + window.location.host + projectName + "/login.html";
 //获取url中的参数
